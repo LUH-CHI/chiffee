9b0c624a2cc9c279a87766d5289b49b92fcf16bb
Svetlana Ovcharenko <ovcharenko@chi.uni-hannover.de>
2020-02-03 16:06:28 +0100

    Made changes to Admin panel UI

--- admin.py
+++ admin.py
@@ -2,7 +2,26 @@ from django.contrib import admin
 
 # Register your models here.
 
+from .models import Deposit, Employee, Product, Purchase
 
-from .models import Product, Buy, Employee, Deposit
 
-admin.site.register((Product, Buy, Employee, Deposit))
+class DepositAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Deposit._meta.fields]
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Employee._meta.fields]
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Product._meta.fields]
+
+
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Purchase._meta.fields]
+
+
+admin.site.register(Deposit, DepositAdmin)
+admin.site.register(Employee, EmployeeAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Purchase, PurchaseAdmin)
